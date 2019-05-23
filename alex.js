b63009eee085b3ec726291ee53bf0fbafeb6775a
Guilherme <jgvarandas@gmail.com>
2019-05-22 23:20:42 -0400

    Organizing files + sweeping unused code

---
+++ alex.js
@@ -0,0 +1,122 @@
+export default {
+  name: "Alexandre Taitson Moreira",
+  occupation: "Developer",
+  address: "128 Fairview Mall Drive",
+  email: "moreira.alx@gmail.com",
+  phone: "(647) 833 5050)",
+  linkedin: "moreiraalx",
+  skills: [
+    { name: "Unity", time: "8" },
+    { name: "Game Dev", time: "8" },
+    { name: "C#", time: "8" },
+    { name: ".NET", time: "8" },
+    { name: "SQL", time: "8" },
+    { name: "Javascript", time: "8" },
+    { name: "Entity Framework", time: "6" },
+    { name: "JSON", time: "5" },
+    { name: "Git", time: "4" },
+    { name: "SCRUM", time: "3" }
+  ],
+  workExperience: [
+    {
+      job: "Character Designer",
+      company: "Bioszard Dev",
+      type: "Contract",
+      start: "Feb 2019",
+      end: "Apr 2019",
+      location: "Brasilia, DF/Brazil",
+      activities: [
+        "Remotely designed characters for the Brazilian game Bioszard Corporation, to be released on May 3rd, 2019."
+      ]
+    },
+    {
+      job: ".NET Analyst",
+      company: "BASIS Technology",
+      type: "Permanent, Fulltime",
+      start: "Aug 2016",
+      end: "Aug 2017",
+      location: "Brasilia, DF/Brazil",
+      activities: [
+        "Developed and maintained government management softwares based on client requirements using RUP methodologies, C#, VB, Javascript and SQL.",
+        "Discussed features and gathered requirements with the client to improve existing softwares."
+      ]
+    },
+    {
+      job: "Game Developer",
+      company: "TIU Game Studios",
+      type: "Contract",
+      start: "Mar 2015",
+      end: "Jan 2016",
+      location: "Brasilia, DF/Brazil",
+      activities: [
+        "Developed the game Colabonacao using Unity3D and published for Android platform, in Brazilian Google Play Store. As the game was funded by the government, it was focused on Brazilian players and market.",
+        "https://www.youtube.com/watch?v=QLpIQCDHPmk"
+      ]
+    },
+    {
+      job: ".NET Analyst",
+      company: "Autotrac",
+      type: "Permanent, Fulltime",
+      start: "Apr 2015",
+      end: "Apr 2016",
+      location: "Brasilia, DF/Brazil",
+      activities: [
+        "Re-wrote a legacy system optimizing the threads making it scalable and fixed the bottleneck that prevented the system from getting more customers. Used C#, Javascript and SQL.",
+        "Updated the user interface reflecting system new features using geolocation and information received from mobile GPS."
+      ]
+    },
+    {
+      job: "System Analyst",
+      company: "Indra Company",
+      type: "Permanent, Fulltime",
+      start: "Feb 2014",
+      end: "Mar 2015",
+      location: "Brasilia, DF/Brazil",
+      activities: [
+        "Maintained legacy systems and improved it as the client requested using .NET 2.0, VB.NET, Javascript, Java, SQL.",
+        "Discussed the client’s needs to gather requirements to improve existing softwares."
+      ]
+    },
+    {
+      job: ".NET Analyst",
+      company: "Pan American Health Organization",
+      type: "Permanent, Fulltime",
+      start: "Apr 2010",
+      end: "Nov 2013",
+      location: "Brasilia, DF/Brazil",
+      activities: [
+        "Database modeling (ERM)",
+        "Gathered requirements with clients from different PAHO areas",
+        "Documented requirements for homologation",
+        "Developed management softwares based on documentation",
+        "Developed security system to manage permissions for each used integrated with Active Directory"
+      ]
+    },
+    {
+      job: "Game Designer/Developer",
+      company: "Behold Studios",
+      type: "Permanent, Fulltime",
+      start: "Jan 2010",
+      end: "Jul 2010",
+      location: "Brasilia, DF/Brazil",
+      activities: [
+        'Design and develop spin-off mini-games based on the company\'s first game: "The Gravedigger" using C# on Unity3D.',
+        "Performed Game Tests looking for bugs or balancing issues."
+      ]
+    }
+  ],
+  certifications: [
+    { name: "Microsoft Certified Professional", year: "2012" },
+    {
+      name: "Microsoft Technology Associate - Web Dev Fundamentals (98-363)",
+      year: "2011"
+    }
+  ],
+  education: {
+    name: "Computer Science BSc",
+    college: "Instituto de Ensino Superior de Brasilia",
+    location: "Brasilia, DF Brazil",
+    start: "Jan 2008",
+    end: "Jan 2010"
+  }
+};
